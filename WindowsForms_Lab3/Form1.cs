99c5b9a6f7e62489eeeb9037deacd4531039e38c
alexsudochakov37@gmail.com <alexsudochakov37@gmail.com>
2020-09-20 15:06:54 +0300

    Добавил коменты

--- WindowsForms_Lab3/Form1.cs
+++ WindowsForms_Lab3/Form1.cs
@@ -89,106 +89,107 @@ namespace WindowsForms_Lab3
 
     class Fraction
     {
+        // Объявдем переменные
         int numerator;
         int denomirator;
-
+        // Свойства переменных
         public int Numerator { get => numerator; set => numerator = value; }
         public int Denomirator { get => denomirator; set => denomirator = (value != 0 ? value : 1); }
-
+        // Конструктор без параметров
         public Fraction()
         {
             this.denomirator = 1;
         }
-
+        // Конструктор с 1 параметром
         public Fraction(int numerator)
         {
             this.numerator = numerator;
             this.denomirator = 1;
         }
-
+        // Конструктор с 2 параметрами
         public Fraction(int numerator, int denomirator) : this(numerator)
         {
             Denomirator = denomirator;
         }
-
+        // Метод вывода дроби
         override public String ToString()
         {
             return " " + this.numerator + "\n-----\n" + " " + this.denomirator;
         }
-
+        // Перегрузка оператора сложения
         public static Fraction operator +(Fraction drop1, Fraction drop2)
         {
-            Fraction temp = new Fraction();
+            Fraction temp = new Fraction(); // Создаем объект класса 
 
-            temp.denomirator = drop1.denomirator * drop2.denomirator;
-            temp.numerator = drop1.numerator * drop2.denomirator + drop2.numerator * drop1.denomirator;
+            temp.denomirator = drop1.denomirator * drop2.denomirator; // Перемножаем два знаменателя и присваиваем значение знаменателю объекта
+            temp.numerator = drop1.numerator * drop2.denomirator + drop2.numerator * drop1.denomirator; // Суммируем умножения знаменателей и числитей и присваиваем значение чеслителю объекта
 
-            return temp;
+            return temp; // Возвращаем объект
         }
-
+        // Перегрузка оператора разности
         public static Fraction operator -(Fraction drop1, Fraction drop2)
         {
-            Fraction temp = new Fraction();
+            Fraction temp = new Fraction(); // Создаем объект класса 
 
-            temp.denomirator = drop1.denomirator * drop2.denomirator;
-            temp.numerator = drop1.numerator * drop2.denomirator - drop2.numerator * drop1.denomirator;
+            temp.denomirator = drop1.denomirator * drop2.denomirator; // Перемножаем два знаменателя и присваиваем значение знаменателю объекта
+            temp.numerator = drop1.numerator * drop2.denomirator - drop2.numerator * drop1.denomirator; // Отнимае умножения знаменателей и числитей и присваиваем значение чеслителю объекта
 
-            return temp;
+            return temp; // Возвращаем объект
         }
-
+        // Перегрузка оператора умножения
         public static Fraction operator *(Fraction drop1, Fraction drop2)
         {
-            Fraction temp = new Fraction();
+            Fraction temp = new Fraction(); // Создаем объект класса
 
-            temp.numerator = drop1.numerator * drop2.numerator;
-            temp.denomirator = drop1.denomirator * drop2.denomirator;
+            temp.numerator = drop1.numerator * drop2.numerator; // Присваиваем переменной объекта умножение двух числителей
+            temp.denomirator = drop1.denomirator * drop2.denomirator; // Присваиваем переменной объекта умножение двух знаменателей
 
-            return temp;
+            return temp; // Возвращаем объект
         }
-
+        // Конструктор переворачивания дроби 
         public Fraction Reverse()
         {
-            Fraction temp = new Fraction();
+            Fraction temp = new Fraction(); // Создаем объект класса
 
-            temp.numerator = this.denomirator;
-            temp.denomirator = this.numerator;
+            temp.numerator = this.denomirator; // Присваиваем переменной объекта значение знаменателя
+            temp.denomirator = this.numerator; // Присваиваем переменной объекта значение числителя
 
-            return temp;
+            return temp; // Возвращаем объект
         }
-
+        // Перегрузка оператора деления
         public static Fraction operator /(Fraction drop1, Fraction drop2)
         {
-            Fraction temp = new Fraction();
+            Fraction temp = new Fraction(); // Создаем объект класса
 
-            temp = drop1 * drop2.Reverse();
+            temp = drop1 * drop2.Reverse(); // Присваиваем объекту умножение двух дробей, одна из которых перевернутая
 
-            return temp;
+            return temp; // Возвращаем объект
         }
-
+        // Метод сокращения дроби
         public void Simplify()
         {
-            int tNumerator = this.numerator;
-            int tDenominator = this.denomirator;
+            int tNumerator = this.numerator; // Создаем переменную и присваиваем ей значение полученого числителя
+            int tDenominator = this.denomirator; // Создаем переменную и присваиваем ей значение полученого знаменателя
 
-            if (tDenominator % tNumerator == 0)
+            if (tDenominator % tNumerator == 0) // Проверка на то, можно ли подилить нацело знаменатель на числитель
             {
-                denomirator /= numerator;
-                numerator = 1;
-                return;
+                denomirator /= numerator; // Делим знаменатель на числитель и присваиваем значение знаменателю
+                numerator = 1; // Присваиваеи 1 числителю
+                return; // Заканчиваем действие метода
             }
-
-            for (int i = 2; i < tNumerator; i++)
+            // Создаем цикл для сокращения
+            for (int i = 2; i < tNumerator; i++) // Начало итератора начинаем с 2, смысла делить на 0 и 1 нет, конец это значие числителя
             {
-                if (tNumerator % i == 0 && tDenominator % i == 0)
+                if (tNumerator % i == 0 && tDenominator % i == 0) // Проверка на целочисленное деления занменателя и числителя на итератор
                 {
-                    tNumerator /= i;
-                    tDenominator /= i;
-                    i--;
+                    tNumerator /= i; // Делим числитель на итератор и присваиваем значение числителю
+                    tDenominator /= i; // Делим знаменитель на итератор и присваиваем значение знаменателю
+                    i--; // Уменьшаем итератор на 1
                 }
             }
 
-            this.numerator = tNumerator;
-            this.denomirator = tDenominator;
+            this.numerator = tNumerator; // Присваиваем полученое значение полученому числителю
+            this.denomirator = tDenominator; // Присваиваем полученое значение полученому знаменателю
         }
     }
 }
